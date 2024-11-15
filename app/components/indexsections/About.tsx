a7bf495be7f516a28706e40c321bebb92faf1007
MrPotatoXx <salinasgiovanni25@gmail.com>
2024-11-15 17:10:59 -0300

    Implementar carga de datos de perfil en el componente About

--- app/components/indexsections/About.tsx
+++ app/components/indexsections/About.tsx
@@ -1,7 +1,22 @@
-export default function About() {
-	return (
-	  <section className="text-center py-20">
-		<p>about</p>
-	  </section>
-	)
+import { getProfile } from '../../lib/gets'
+type ProfileData = {
+	summary: string;
+  }
+export const About = async () => {
+	try {
+	  const profileData = await getProfile()
+	  const { summary }: ProfileData = profileData
+	  return (
+		<section>
+		  <div className="max-w-4xl mx-auto mb-10">
+		  <p className="text-base md:text-lg text-gray-300 leading-relaxed my-4">
+			{summary}
+		  </p>
+		  </div>
+		</section>
+	  )
+	} catch (error) {
+	  console.error('Error fetching profile data:', error)
+	  return <div>Error al cargar el perfil</div>
+	}
   }
