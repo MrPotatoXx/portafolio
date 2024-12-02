2cbb692c98a6d3605cd9b1b9a83f0066d30d20e5
MrPotatoXx <salinasgiovanni25@gmail.com>
2024-12-02 03:12:16 -0300

    Modificar importación del componente Projects para usar exportación nombrada

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from '@/app/components/indexsections/Hero'
 import { About } from '@/app/components/indexsections/About'
 import { Experience } from '@/app/components/indexsections/Experience'
 import { Education } from '@/app/components/indexsections/Education'
-import Projects from '@/app/components/indexsections/Projects'
+import { Projects } from '@/app/components/indexsections/Projects'
 import Courses from '@/app/components/indexsections/Courses'
 import Skills from '@/app/components/indexsections/Skills'
 
