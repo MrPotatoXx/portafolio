3ba286574b928ddab036dc75c88e793c5dc331c2
MrPotatoXx <salinasgiovanni25@gmail.com>
2025-09-06 02:26:50 -0400

    feat: agregar integración de sitemap en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://potatox.me',
-	vite: {
+	integrations: [sitemap()],
+	vite: /** @type {any} */ ({
 	  plugins: [tailwindcss()],
-	},
+	}),
 });
