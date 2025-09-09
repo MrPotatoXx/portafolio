ae2f5c999fcf688e94d885dc742fe1ebf3c70e76
MrPotatoXx <salinasgiovanni25@gmail.com>
2025-09-09 00:21:56 -0300

    feat: agregar configuración de i18n con soporte para múltiples idiomas

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,13 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://potatox.me',
+	i18n: {
+	  locales: ['es', 'en'],
+	  defaultLocale: 'es',
+	  routing: {
+	    prefixDefaultLocale: true,
+	  },
+	},
 	integrations: [sitemap()],
 	vite: /** @type {any} */ ({
 	  plugins: [tailwindcss()],
